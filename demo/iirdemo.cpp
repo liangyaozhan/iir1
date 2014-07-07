aaa74a03f130541f38e2d42b304745d316475f45
yaozhanliang <ivws02@gmail.com>
2014-07-08 07:58:52 +0800

    modify demo

--- demo/iirdemo.cpp
+++ demo/iirdemo.cpp
@@ -11,29 +11,77 @@
 #include <iomanip>
 #include <stdio.h>
 
+#include "wave-signal.h"
+#include <fftw3.h>
+
+int g_global_v;
+
+
+template <typename filter>
+void freqz( filter &ftr,
+            double freq0, double freq1, double freq_step,
+            int fs, int np, int skip,
+            const char *filename )
+{
+    struct wave_signal wave_signal;
+    FILE *fp;
+
+    fp = fopen( filename, "wt" );
+    if ( !fp )
+    {
+        return;
+    }
+
+    fprintf( fp, "freq,rms\n" );
+    wave_signal_init( &wave_signal );
+    wave_signal_set(&wave_signal, 1, fs, np);
+    
+    for ( ; freq0 <= freq1 ; freq0 += freq_step )
+    {
+        double rms;
+        double *d;
+        wave_signal_clear( &wave_signal );
+        wave_signal_add( &wave_signal, 100, 0, freq0, 0 );
+        d = wave_signal.p;
+        for ( int k=0; k<np; k++ )
+        {
+            *d = ftr.filter( *d );
+            d++;
+        }
+        rms = wave_signal_get_rms_skip( &wave_signal, skip);
+        fprintf( fp, "%f,%f\n", freq0, rms );
+    }
+    wave_signal_destroy( &wave_signal );
+    fclose( fp );
+}
+
+
 int main (int,char**)
 {
-	const int order = 3;
+	const int order = 6;
 	Iir::Butterworth::LowPass<order> f;
-	const float samplingrate = 1000; // Hz
-	const float cutoff_frequency = 5; // Hz
+	const float samplingrate = 25600; // Hz
+	const float cutoff_frequency = 35; // Hz
 	f.setup (order, samplingrate, cutoff_frequency);
 	f.reset ();
 	FILE *fimpulse = fopen("lp.dat","wt");
 	for(int i=0;i<1000;i++) 
 	{
 		float a=0;
-		if (i==10) a = 1;
+		if (i>=10) a = 1;
 		float b = f.filter(a);
 		fprintf(fimpulse,"%f\n",b);
 	}
 	fclose(fimpulse);
 	
+	printf( "%d\n", g_global_v );
 	// calculate response at frequency 440 Hz
 	Iir::complex_t response = f.response (440./44100);
 	
 	std::ostringstream os;
 
+    freqz( f, 0, 100, 0.5, samplingrate, samplingrate*2, samplingrate, "abc.csv" );
+    
 	// coefficients
 	int n = f.getNumStages();
 	os << "numStages = " << n << "\n";
